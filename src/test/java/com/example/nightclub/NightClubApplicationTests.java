736184c1f10d3b4fa709f173cc9d7ed76e16a31f
Jaeho <wlsqowogh12@gmail.com>
2025-04-09 16:27:04 +0900

    git 좋아요 엔티티 작성

---
+++ src/test/java/com/example/nightclub/NightClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nightclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
