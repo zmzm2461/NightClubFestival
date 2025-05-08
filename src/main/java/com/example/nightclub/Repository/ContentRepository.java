168ece353c262a0175ac526bc6f67335542d5e1a
WeonJae <kongwonjae123@naver.com>
2025-05-08 15:21:35 +0900

    엔티티,레파지토리,Dto, Controller, service

--- src/main/java/com/example/nightclub/Repository/ContentRepository.java
+++ src/main/java/com/example/nightclub/Repository/ContentRepository.java
@@ -8,4 +8,11 @@ import java.util.Optional;
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
     Optional<Content> findByuserid(String userid);
+
+    // 특정 IP(userid)와 postId에 해당하는 댓글을 찾기 위한 쿼리
+    Optional<Content> findByUseridAndPostId(String userid, Long postId);
+
+    // 특정 IP(userid)와 postId에 해당하는 댓글을 삭제하는 쿼리
+    void deleteByUseridAndPostId(String userid, Long postId);
+
 }
